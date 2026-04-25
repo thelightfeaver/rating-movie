13e3d48b1695588e6860eedc486a1c52779a65ab
Diogenes Monegro <diogenes_mj@hotmail.com>
2026-04-25 18:57:05 -0400

    Implement model training logic with data processing and logging

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -1,5 +1,8 @@
 from pathlib import Path
-
+import pandas as pd
+import mlflow
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 import typer
 from loguru import logger
 from tqdm import tqdm
@@ -17,13 +20,15 @@ def main(
     model_path: Path = MODELS_DIR / "model.pkl",
     # -----------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+    
+    mlflow.autolog()
+    df = pd.read_parquet("")
+    df["success"] = df["revenue"] > df["bugdet"] * 2
+    x = df.drop(["success"])
+    y = df["success"]
+    x_train, y_train, x_test, y_test = train_test_split(x, y)
+    lr = LogisticRegression()
+    lr.fit(x_train,y_train)
 
 
 if __name__ == "__main__":
